5b44c166758affa5c44d6f0a6bb1fad18bbc9365
ritesu <ritesu@mail.ru>
2023-01-27 09:10:14 +1100

    Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите длину массива");
+int a = int.Parse(Console.ReadLine()?? "0");
+int[] numbers = new int [a];
+for (int i = 0; i < numbers.Length; i++)
+{
+    int num = new Random().Next(20, 25);
+    numbers[i] = num;
+}
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] == 20)
+    {
+        numbers[i] = 20 + 180;
+        break;
+    }
+}
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
